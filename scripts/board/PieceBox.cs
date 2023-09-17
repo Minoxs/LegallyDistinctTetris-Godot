4088d5b91dd0400e1d48a36947ad5b8ead4f5d27
Mino <dev@mino.one>
2023-09-17 18:32:50 -0300

    Back to janky

--- scripts/board/PieceBox.cs
+++ scripts/board/PieceBox.cs
@@ -3,12 +3,26 @@ using Godot;
 namespace LegallyDistinctTetris.scripts.board;
 
 [GlobalClass]
-public partial class PieceBox : Node2D {
+public partial class PieceBox : TileMap {
     private Piece _heldPiece;
+
+    private void Render() {
+        Clear();
+        foreach (var cell in _heldPiece.CellsPosition) SetCell(0, cell, 0, _heldPiece.Value);
+    }
 
     public Piece Swap(Piece piece) {
         var aux = _heldPiece;
         _heldPiece = piece.Clone().ResetPosition();
+        Render();
         return aux;
+    }
+
+    public override void _Ready() {
+        for (var i = 0; i < 1; i++) AddLayer(0);
+        var parent = GetNode<TileMap>("..");
+        if (parent == null) return;
+        TileSet = parent.TileSet;
+        CellQuadrantSize = parent.CellQuadrantSize;
     }
 }
