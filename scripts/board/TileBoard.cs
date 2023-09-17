7e97854037c5bf33f84e456e5b04ee89161787d8
Mino <dev@mino.one>
2023-09-17 16:48:59 -0300

    Playing around the editor

--- scripts/board/TileBoard.cs
+++ scripts/board/TileBoard.cs
@@ -3,8 +3,17 @@ using Godot;
 
 namespace LegallyDistinctTetris.scripts.board;
 
+[Tool]
 [GlobalClass]
 public partial class TileBoard : TileMap {
+    // Update board in the editor
+    [Export]
+    public bool UpdateBoardInEditor;
+
+    // Black piece used in the background layer
+    [Export]
+    public Vector2I BgBlockAtlasCoord = Vector2I.Right;
+
     // How many ticks touching the floor before the piece is placed
     [Export(PropertyHint.Range, "0,10,1,or_greater")]
     public int FloorTicks = 3;
@@ -84,7 +93,29 @@ public partial class TileBoard : TileMap {
     private void CreateBoard() {
         _spawn = new Vector2I(BoardSize.X / 2, 0);
         _realBoard = new PieceBoard(BoardSize);
-        GenerateNewPiece();
+    }
+
+    private void UpdateBoardVisuals() {
+        Position = new Vector2(CellQuadrantSize * Scale.X, 0) - CanvasSize / 2;
+
+        // Make sure there are 3 layers in the thing
+        // 0 - Background layer
+        // 1 - Fixed blocks layer
+        // 2 - Moving piece layer
+        for (var i = GetLayersCount(); i < 3; i++) AddLayer(i);
+
+        Clear();
+        // Create walls
+        for (var y = 0; y < BoardSize.Y; y++) {
+            SetCell(0, new Vector2I(-1, y), 0, BgBlockAtlasCoord);
+            SetCell(0, new Vector2I(BoardSize.X, y), 0, BgBlockAtlasCoord);
+        }
+
+        // Create floor
+        for (var x = -1; x < BoardSize.X + 1; x++) SetCell(0, new Vector2I(x, BoardSize.Y), 0, BgBlockAtlasCoord);
+
+        // Offset by one cell to start board in (0, 0), then move center point to (0, 0)
+        Position = new Vector2I(CellQuadrantSize, 0) - CanvasSize / 2;
     }
 
     private void Tick(double delta) {
@@ -122,13 +153,20 @@ public partial class TileBoard : TileMap {
     }
 
     public override void _Ready() {
+        if (Engine.IsEditorHint()) return;
         CreateBoard();
+        GenerateNewPiece();
         Render();
     }
 
     public override void _Process(double delta) {
-        ProcessInput();
-        Tick(delta);
-        Render();
+        if (Engine.IsEditorHint()) {
+            if (UpdateBoardInEditor) UpdateBoardVisuals();
+        }
+        else {
+            ProcessInput();
+            Tick(delta);
+            Render();
+        }
     }
 }
