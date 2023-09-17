75a984afad9b5edb8ea1aeaed4c7fb32448d974c
Mino <dev@mino.one>
2023-09-17 16:54:31 -0300

    Move editor stuff to another script

--- scripts/board/TileBoard.cs
+++ scripts/board/TileBoard.cs
@@ -3,17 +3,8 @@ using Godot;
 
 namespace LegallyDistinctTetris.scripts.board;
 
-[Tool]
 [GlobalClass]
 public partial class TileBoard : TileMap {
-    // Update board in the editor
-    [Export]
-    public bool UpdateBoardInEditor;
-
-    // Black piece used in the background layer
-    [Export]
-    public Vector2I BgBlockAtlasCoord = Vector2I.Right;
-
     // How many ticks touching the floor before the piece is placed
     [Export(PropertyHint.Range, "0,10,1,or_greater")]
     public int FloorTicks = 3;
@@ -33,11 +24,6 @@ public partial class TileBoard : TileMap {
     // Piece being held
     [Export]
     public PieceBox HeldPiece;
-
-    // Size required to render board
-    // 2 for walls + Board width
-    // 1 for floor + Board heigth
-    public Vector2I CanvasSize => new Vector2I(2 + BoardSize.X, 1 + BoardSize.Y) * CellQuadrantSize * (Vector2I)Scale;
 
     // Current piece being moved
     private Piece _pieceCur;
@@ -95,29 +81,6 @@ public partial class TileBoard : TileMap {
         _realBoard = new PieceBoard(BoardSize);
     }
 
-    private void UpdateBoardVisuals() {
-        Position = new Vector2(CellQuadrantSize * Scale.X, 0) - CanvasSize / 2;
-
-        // Make sure there are 3 layers in the thing
-        // 0 - Background layer
-        // 1 - Fixed blocks layer
-        // 2 - Moving piece layer
-        for (var i = GetLayersCount(); i < 3; i++) AddLayer(i);
-
-        Clear();
-        // Create walls
-        for (var y = 0; y < BoardSize.Y; y++) {
-            SetCell(0, new Vector2I(-1, y), 0, BgBlockAtlasCoord);
-            SetCell(0, new Vector2I(BoardSize.X, y), 0, BgBlockAtlasCoord);
-        }
-
-        // Create floor
-        for (var x = -1; x < BoardSize.X + 1; x++) SetCell(0, new Vector2I(x, BoardSize.Y), 0, BgBlockAtlasCoord);
-
-        // Offset by one cell to start board in (0, 0), then move center point to (0, 0)
-        Position = new Vector2I(CellQuadrantSize, 0) - CanvasSize / 2;
-    }
-
     private void Tick(double delta) {
         _tickStep += delta;
         if (_tickStep < TickRate) return;
@@ -153,20 +116,14 @@ public partial class TileBoard : TileMap {
     }
 
     public override void _Ready() {
-        if (Engine.IsEditorHint()) return;
         CreateBoard();
         GenerateNewPiece();
         Render();
     }
 
     public override void _Process(double delta) {
-        if (Engine.IsEditorHint()) {
-            if (UpdateBoardInEditor) UpdateBoardVisuals();
-        }
-        else {
-            ProcessInput();
-            Tick(delta);
-            Render();
-        }
+        ProcessInput();
+        Tick(delta);
+        Render();
     }
 }
---
+++ scripts/board/TileBoardBuilder.cs
@@ -0,0 +1,52 @@
+using Godot;
+
+namespace LegallyDistinctTetris.scripts.board;
+
+[Tool]
+[GlobalClass]
+public partial class TileBoardBuilder : TileBoard {
+    // Update board in the editor
+    [Export]
+    public bool UpdateBoardInEditor;
+
+    // Black piece used in the background layer
+    [Export]
+    public Vector2I BgBlockAtlasCoord = Vector2I.Right;
+
+    // Size required to render board
+    // 2 for walls + Board width
+    // 1 for floor + Board heigth
+    public Vector2I CanvasSize => new Vector2I(2 + BoardSize.X, 1 + BoardSize.Y) * CellQuadrantSize * (Vector2I)Scale;
+
+    private void UpdateBoardVisuals() {
+        Position = new Vector2(CellQuadrantSize * Scale.X, 0) - CanvasSize / 2;
+
+        // Make sure there are 3 layers in the thing
+        // 0 - Background layer
+        // 1 - Fixed blocks layer
+        // 2 - Moving piece layer
+        for (var i = GetLayersCount(); i < 3; i++) AddLayer(i);
+
+        Clear();
+        // Create walls
+        for (var y = 0; y < BoardSize.Y; y++) {
+            SetCell(0, new Vector2I(-1, y), 0, BgBlockAtlasCoord);
+            SetCell(0, new Vector2I(BoardSize.X, y), 0, BgBlockAtlasCoord);
+        }
+
+        // Create floor
+        for (var x = -1; x < BoardSize.X + 1; x++) SetCell(0, new Vector2I(x, BoardSize.Y), 0, BgBlockAtlasCoord);
+
+        // Offset by one cell to start board in (0, 0), then move center point to (0, 0)
+        Position = new Vector2I(CellQuadrantSize, 0) - CanvasSize / 2;
+    }
+
+    public override void _Ready() {
+        if (!Engine.IsEditorHint()) base._Ready();
+    }
+
+    public override void _Process(double delta) {
+        if (UpdateBoardInEditor) UpdateBoardVisuals();
+        if (!Engine.IsEditorHint()) base._Process(delta);
+    }
+}
